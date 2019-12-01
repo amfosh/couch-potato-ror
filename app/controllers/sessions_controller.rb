e2724910eb44a7983ae190e49472dc2694e2ff66
alicia <afasciocco@gmail.com>
2019-12-01 08:20:14 -0500

    login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,8 @@ class SessionsController < ApplicationController
   end
 
   def create 
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(password: params[:user][:password])
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
-    has_secure_password
     has_many :shows
     has_many :notes, through: :shows 
     has_many :statuses, through: :shows
-    validates :email, uniqueness: true, presence: true
-    validates :username, :password, presence: true
+    validates :username, uniqueness: true,  presence: true
+    validates :email, presence: true
+    has_secure_password
 
-    def self.find_or_create_by_omniauth(auth_hash)
-        self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
-          user.username = auth_hash["info"]["name"]
-          user.provider = auth_hash["provider"]
-          user.uid = auth_hash["uid"]
-          user.password = SecureRandom.hex
-        end
-      end
+    # def self.find_or_create_by_omniauth(auth_hash)
+    #     self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
+    #       user.username = auth_hash["info"]["name"]
+    #       user.provider = auth_hash["provider"]
+    #       user.uid = auth_hash["uid"]
+    #       user.password = SecureRandom.hex
+    #     end
+    #   end
 end
