8443a38043b2240b843cf324e7d2e59280efab9e
alicia <afasciocco@gmail.com>
2019-12-01 16:30:05 -0500

    list showing up with add a note feature

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,7 @@ class Show < ApplicationRecord
     belongs_to :user
     has_many :notes
     has_one :status
+    validates :show_title, presence: true 
 
     def self.my_shows(user)
         where(user_id: user)
