a5becc842fcfddedc987a6b8e4648d9ff61a6884
alicia <afasciocco@gmail.com>
2019-12-03 19:05:49 -0500

    added another error

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,11 +4,16 @@ class Show < ApplicationRecord
     has_many :notes
     has_one :status
     validates :show_title, presence: true 
+    validate :not_a_duplicate
 
     def self.my_shows(user)
         where(user_id: user)
     end
 
-    def watched 
+    def not_a_duplicate
+        show = Show.find_by(show_title: show_title) 
+        if !!show && show != self
+            errors.add(:show_title, 'is already on your list')
+        end
     end
 end
