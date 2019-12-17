2e06080de7ea740c994fda76053b6c0ca20c6b31
alicia <afasciocco@gmail.com>
2019-12-17 18:11:49 -0500

    611pm

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ApplicationRecord
     belongs_to :show
-    belongs_to :user
+    belongs_to :user, optional: true
     # has_many :users, through: :shows
     accepts_nested_attributes_for :show
     validates :content, presence: true
