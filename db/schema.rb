c64f57a415cdcb7760b272eb1c707f2462cdd6ed
amfosh <afasciocco@gmail.com>
2020-07-26 11:53:25 -0400

    sql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_03_215647) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
